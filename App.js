148f806e3caa4159e9991826922a0459a74e1469
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2020-09-17 14:53:59 +0700

    WorkshopI

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet,
+         Text,
+         View,
+         Image,
+         TextInput,
+         Button
+        } from 'react-native';
+import MyButton from './components/MyButton.js'
 
 export default function App() {
+  const [ input, setInput ] = useState('')
+  const [ color, setColor ] = useState('red')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={{ uri: 'https://www.igeargeek.com/_nuxt/img/835647d.png'}}
+        style={styles.logo}
+      />
+      <Text style={styles.title}> Hello, world </Text>
+      <TextInput 
+        value={input}
+        onChangeText={(text) => { setInput(text) }}
+        placeholder='Input here'
+        style={styles.input}
+      />
+      <Text style={{fontSize: 18, marginTop:20, color: color}}>{input}</Text>
+      <View style={styles.buttonLayout}>
+        <MyButton title='Red' color='red' onChangeColor={(color) => {setColor(color)}} />
+        <MyButton title='Blue' color='blue' onChangeColor={(color) => {setColor(color)}}/>
+        <MyButton title='Green' color='green' onChangeColor={(color) => {setColor(color)}}/>
+      </View>
     </View>
   );
 }
@@ -18,4 +41,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 150,
+    height: 150,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    width: '100%',
+    marginTop: 20,
+  },
+  buttonLayout: {
+    flexDirection: 'row',
+    marginTop: 20,
+  }
+
 });
